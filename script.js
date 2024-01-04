301ccb258e2b408f4e670814f7f3ce91719ed645
༺ᎷΛRƵIEL60D༻ <140960098+Marziel60D@users.noreply.github.com>
2024-01-04 11:15:30 +0800

    an update!

--- script.js
+++ script.js
@@ -26,6 +26,5 @@ function clickListener(event) {
 let submitButton = document.querySelector('#submit-button');
 
 submitButton.addEventListener('click', clickListener);
-console.log("test print")
 
-//added some comments
+
